fb102079dd0d890efd387577ee6a11c12beb4fe0
SalimanDolev <44755169+SalimanDolev@users.noreply.github.com>
2019-04-15 16:52:54 +0300

    Add files via upload

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -1018,8 +1018,9 @@ PhysicalNumber& PhysicalNumber::operator+=(const PhysicalNumber& rhs)
          value = value + (rhs.value / 1000);
          break;
          }
+         throw "no sulotion"; break;
          default:
-         break;
+         throw "no sulotion"; break;
        }
      return *this;
 }
@@ -1131,6 +1132,8 @@ PhysicalNumber& PhysicalNumber::operator-=(const PhysicalNumber& rhs)
          value = value - (rhs.value / 1000);
          break;
          }
+         throw "no sulotion"; break;
+         default:
          throw "no sulotion"; break;
        }
      return *this;
